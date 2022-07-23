6f07f6ebaccc696ffc208091504edaea4401c2af
Abhishek Patil <abhishekpatil@Abhisheks-MacBook-Pro-2.local>
2022-07-23 15:02:12 +0530

    removed psql conn strings

--- users-api-with-orm/repository/postgres_conn.go
+++ users-api-with-orm/repository/postgres_conn.go
@@ -9,10 +9,10 @@ import (
 
 func SetupDB() *gorm.DB {
 
-	// dbURL := "postgres://aplos_qa:Aplos2022@aplos-qa.c6n8utnxko28.ap-south-1.rds.amazonaws.com:5432/patil_db"
+	// dbURL := "postgres://username:pass@dns:5432/patil_db"
 	// db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
-	dsn := "host=aplos-qa.c6n8utnxko28.ap-south-1.rds.amazonaws.com user=aplos_qa password=Aplos2022 dbname=patil_db port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	dsn := "host=dns user=username password=pass dbname=patil_db port=5432 sslmode=disable TimeZone=Asia/Kolkata"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
